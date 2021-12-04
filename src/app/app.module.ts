ed7c25f6ec73af65cf369a64231ddb6bcabc8919
Abdulrahman Mohammed Alomar <alomardev@gmail.com>
2021-12-04 17:43:01 +0300

    build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,6 +21,7 @@ import { ScpLoadingOverlayModule } from './directives/loading-overlay/loading-ov
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
